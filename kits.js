53f1f02fe1e7561ed04789b046f6245e2f7c3e1f
nausofficial <49093506+nausofficial@users.noreply.github.com>
2026-02-16 01:05:35 +0300

    Create kits.js
    kits 🥇

---
+++ kits.js
@@ -0,0 +1,45 @@
+const KITS = [
+    {
+        id: 'starter',
+        name: 'STARTER KIT',
+        price: 'FREE',
+        image: 'https://i.imgur.com/8QZ7Z7u.png', // Örnek resim (Değiştir)
+        tebexPackageId: 'https://basicrustsitestore.tebex.io/package/123456', // Tebex linki
+        contents: [
+            "1x Stone Hatchet",
+            "1x Stone Pickaxe",
+            "1000x Wood",
+            "50x Scrap"
+        ],
+        color: '#cd412b' // Kart rengi (Turuncu)
+    },
+    {
+        id: 'vip',
+        name: 'VIP KIT',
+        price: '$10.00',
+        image: 'https://i.imgur.com/5XqZ7Z7.png',
+        tebexPackageId: 'https://basicrustsitestore.tebex.io/package/654321',
+        contents: [
+            "1x Assault Rifle (AK-47)",
+            "1x Metal Chestplate",
+            "1x Metal Facemask",
+            "4x Syringe",
+            "Skip Queue"
+        ],
+        color: '#f1c40f' // Kart rengi (Altın)
+    },
+    {
+        id: 'raider',
+        name: 'RAIDER KIT',
+        price: '$25.00',
+        image: 'https://i.imgur.com/9YxZ7Z7.png',
+        tebexPackageId: 'https://basicrustsitestore.tebex.io/package/987654',
+        contents: [
+            "10x C4 Explosive",
+            "1x Rocket Launcher",
+            "50x High Velocity Rocket",
+            "Full Metal Gear Set"
+        ],
+        color: '#e74c3c' // Kart rengi (Kırmızı/Tehlikeli)
+    }
+];
